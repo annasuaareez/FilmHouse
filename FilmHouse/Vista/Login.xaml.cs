131cb5ab04f634ea60b1db06db4b04af21c2fbdc
annas <a.suarez.villaescusa@gmail.com>
2024-01-25 23:08:55 +0100

    Cut, Copy and Paster

--- FilmHouse/Vista/Login.xaml.cs
+++ FilmHouse/Vista/Login.xaml.cs
@@ -1,9 +1,57 @@
+
 namespace FilmHouse.Vista;
 
 public partial class Login : ContentPage
 {
-	public Login()
-	{
-		InitializeComponent();
-	}
+    public Login()
+    {
+        InitializeComponent();
+    }
+
+    private async void TapCopyCutPaste(object sender, EventArgs e)
+    {
+        var resultado = await DisplayActionSheet("Actions", "Cancel", null, "Cut", "Copy", "Paste");
+        Label valor = (Label)sender;
+        string valorTexto = valor.Text;
+
+        switch (resultado)
+        {
+            case "Cut":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        await Clipboard.SetTextAsync(Username.Text);
+                        Username.Text = "";
+                        break;
+                    case "Password":
+                        await Clipboard.SetTextAsync(Pass.Text);
+                        Pass.Text = "";
+                        break;
+                }
+                break;
+            case "Copy":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        await Clipboard.SetTextAsync(Username.Text);
+                        break;
+                    case "Password":
+                        await Clipboard.SetTextAsync(Pass.Text);
+                        break;
+                }
+                break;
+            case "Paste":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        Username.Text += await Clipboard.GetTextAsync();
+                        break;
+                    case "Password":
+                        Pass.Text += await Clipboard.GetTextAsync();
+                        break;
+                }
+                break;
+        }
+    }
 }
+
--- FilmHouse/Vista/Registro.xaml.cs
+++ FilmHouse/Vista/Registro.xaml.cs
@@ -6,4 +6,79 @@ public partial class Registro : ContentPage
 	{
 		InitializeComponent();
 	}
+    private async void TapCopyCutPaste(object sender, EventArgs e)
+    {
+        var resultado = await DisplayActionSheet("Actions", "Cancel", null, "Cut", "Copy", "Paste");
+        Label valor = (Label)sender;
+        string valorTexto = valor.Text;
+
+        switch (resultado)
+        {
+            case "Cut":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        await Clipboard.SetTextAsync(Nombre.Text);
+                        Nombre.Text = "";
+                        break;
+                    case "Age":
+                        await Clipboard.SetTextAsync(Edad.Text);
+                        Edad.Text = "";
+                        break;
+                    case "Username":
+                        await Clipboard.SetTextAsync(Usuario.Text);
+                        Usuario.Text = "";
+                        break;
+                    case "Password":
+                        await Clipboard.SetTextAsync(Contraseña.Text);
+                        Contraseña.Text = "";
+                        break;
+                    case "Repeat Pasword":
+                        await Clipboard.SetTextAsync(Contraseña2.Text);
+                        Contraseña2.Text = "";
+                        break;
+                }
+                break;
+            case "Copy":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        await Clipboard.SetTextAsync(Nombre.Text);
+                        break;
+                    case "Age":
+                        await Clipboard.SetTextAsync(Edad.Text);
+                        break;
+                    case "Username":
+                        await Clipboard.SetTextAsync(Usuario.Text);
+                        break;
+                    case "Password":
+                        await Clipboard.SetTextAsync(Contraseña.Text);
+                        break;
+                    case "Repeat Pasword":
+                        await Clipboard.SetTextAsync(Contraseña2.Text);
+                        break;
+                }
+                break;
+            case "Paste":
+                switch (valorTexto)
+                {
+                    case "Name":
+                        Nombre.Text += await Clipboard.GetTextAsync();
+                        break;
+                    case "Age":
+                        Edad.Text += await Clipboard.GetTextAsync();
+                        break;
+                    case "Username":
+                        Usuario.Text += await Clipboard.GetTextAsync();
+                        break;
+                    case "Password":
+                        Contraseña.Text += await Clipboard.GetTextAsync();
+                        break;
+                    case "Repeat Pasword":
+                        Contraseña2.Text += await Clipboard.GetTextAsync();
+                        break;
+                }
+                break;
+        }
+    }
 }
